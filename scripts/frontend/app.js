b4c90bcf24bc52de29a5f7c98eeec69a86fecc0d
Nadav Tasger <tashernadav@gmail.com>
2019-07-31 18:57:12 +0300

    PHP chat v2 impl

--- scripts/frontend/app.js
+++ scripts/frontend/app.js
@@ -1,5 +1,16 @@
-function load() {
-    // App Load Code
+const CHAT_ENDPOINT = "scripts/backend/chat/v2/chat.php";
+
+function load(loggedIn) {
+    view("app");
+    if (loggedIn) {
+        view("home");
+        transition("home", IN);
+    } else {
+        transition("nologin", IN);
+    }
 }
 
-// App Code
+function loadChatList() {
+    api(CHAT_ENDPOINT, "chat", "list", {}, (success, result, error) => {
+    }, accounts_fill());
+}
